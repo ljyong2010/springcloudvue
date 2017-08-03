8a33b2ff021af4e7da35d56ab195bbba663e36f1
Administrator <ljyong2010@126.com>
2017-08-03 23:44:54 +0800

    Initial commit

---
+++ simple-service/src/main/java/yf/liu/service/contants/Constant.java
@@ -0,0 +1,14 @@
+package yf.liu.service.contants;
+
+public class Constant {
+    //授权key
+    public final static String AUTH_KEY = "authKey";
+    //sessionid
+    public final static String SESSION_ID = "sessionId";
+    //登录用户对象
+    public final static String LOGIN_ADMIN_USER = "login_admin_user";
+    //安全密钥
+    public static String SECRET_KEY = "#rSgRKVunlLUMopFWbEevOy9";
+    //菜单分类ICON
+    public final static String[] ICON = new String[]{"  |","  ├─","  └─"};
+}
